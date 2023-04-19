53fdf931c666d372f21db4025552aa5145b748f8
Duy Anh Nguyen <118113791+duyanhsoict15@users.noreply.github.com>
2023-04-19 11:43:07 +0700

    Add files via upload

---
+++ hephuongtrinh.java
@@ -0,0 +1,43 @@
+package Java;
+import java.util.Scanner;
+
+public class hephuongtrinh {
+    public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Giai he phuong trinh bac nhat 2 an x1, x2:");
+    System.out.println("a11.x1 + a12.x2 = b1");
+    System.out.println("a21.x1 + a22.x2 = b2");
+
+    System.out.println("Nhap gia tri a11: ");
+    double a11 = scanner.nextDouble();
+    System.out.println("Nhap gia tri a12: ");
+    double a12 = scanner.nextDouble();
+    System.out.println("Nhap gia tri a21: ");
+    double a21 = scanner.nextDouble();
+    System.out.println("Nhap gia tri a22: ");
+    double a22 = scanner.nextDouble();
+    System.out.println("Nhap gia tri b1: ");
+    double b1 = scanner.nextDouble();
+    System.out.println("Nhap gia tri b2: ");
+    double b2 = scanner.nextDouble();
+
+    double determinant = a11*a22 - a21*a12;
+
+    if(determinant == 0){
+        if((a11/a12 == a21/a22) && (a11 / a12 == b1/b2)){
+            System.out.println("Infinite solution!");
+        } else {
+            System.out.println("No solution satisfied!");
+        }
+    } else {
+        double x1 = (( b1 * a22 - b2 * a12) / determinant);
+        double x2 = (( b2 * a11 - b1 * a21) / determinant);
+
+        System.out.println("Solution: ");
+        System.out.println("x1 = " + x1);
+        System.out.println("x2 = " + x2);
+    }
+    scanner.close();
+    }
+}
